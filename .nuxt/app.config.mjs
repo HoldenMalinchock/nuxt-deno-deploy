38ea22cde9ee8b72660b678b5dbf88206dca7032
Holden Malinchock <h.malinchock@verizon.net>
2023-05-25 21:29:46 -0500

    Use correct npm build and push the .nuxt

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/holdenmalinchock/dev/nuxt-deno-deploy/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
